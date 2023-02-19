3034680339faae2e6eadf9b1b1c47eb11fff03c7
Arockianeopolean <42064263+Arockianeopolean@users.noreply.github.com>
2023-02-19 19:10:31 +0530

    Update 06.Input Statement.py

--- Day2/06.Input Statement.py
+++ Day2/06.Input Statement.py
@@ -1,5 +1,5 @@
 firstname = input("Enter the First Name:") #To Get User's First Name
-print(firstname)
-print(type(firstname))
+#print(firstname)
+#print(type(firstname))
 lastname = input("Enter the Last Name:") #To Get User's Last Name
 print("User Name:", firstname + " " + lastname)
