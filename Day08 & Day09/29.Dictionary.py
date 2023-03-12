fed28570d4b3448224c84fd3c399bdd0664c160a
Arockianeopolean <42064263+Arockianeopolean@users.noreply.github.com>
2023-03-12 18:45:07 +0530

    Update 29.Dictionary.py

--- Day08 & Day09/29.Dictionary.py
+++ Day08 & Day09/29.Dictionary.py
@@ -1,18 +1,23 @@
 # Dictionary with keys and values of different data types
 numbers = {1: "One", 2: "Two", 3: "Three"}
 print(numbers)
+
+# Access Dict. with keys
 print(numbers[1])
 
+# Add new key-value pair
 numbers[4] = "Four"
 print(numbers)
 
+# Changing the value of key
 numbers[3] = "No-3"
 print(numbers)
 
+# Deleting the key-value
 del numbers[3]
 print(numbers)
 
-#Loop
+#Loop with dictionary keys & values 
 for key,value in numbers.items():
     print(key,":",value)
 
