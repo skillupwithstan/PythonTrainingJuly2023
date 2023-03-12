bdbe6eb7b9e4ca0e4a90dfdbe70290b5e5b30636
Arockianeopolean <42064263+Arockianeopolean@users.noreply.github.com>
2023-03-12 17:50:26 +0530

    Update 27.Tuples.py

--- Day08 & Day09/27.Tuples.py
+++ Day08 & Day09/27.Tuples.py
@@ -1,9 +1,18 @@
+#Create a tuple with values
 sample_tuple = ("Arockia", "Kumar", "Rajesh")
 print(sample_tuple)
+
+sample_tuple1 = (1,2,4)
+print(sample_tuple1)
+
+#Identify the count of elements in a tuple
 print(len(sample_tuple))
+
 #sample_tuple[2] = "Raja"
-sample_tuple = (1,2,4)
-print(sample_tuple)
-print(sample_tuple[0])
-sample_tuple[0] = 5
-print(sample_tuple)
+
+#Access Tuple element
+print(sample_tuple1[0])
+
+# Update Tuple element
+sample_tuple1[0] = 5
+print(sample_tuple1)
